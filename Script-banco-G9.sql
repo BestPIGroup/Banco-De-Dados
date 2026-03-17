05a4a4a2c6373473e0b551e05e76cebf61fb8780
IsabelaTeixeiraRodrigues <isabela.rodrigues@sptech.school>
2026-03-16 21:29:07 -0300

    Modelagem e script coerentes e ajustados

--- Script-banco-G9.sql
+++ Script-banco-G9.sql
@@ -1,6 +1,5 @@
-create database projeto_Argos;
 
-use projeto_Argos;
+
 
 CREATE TABLE unidade (
 	id_unidade INT PRIMARY KEY AUTO_INCREMENT,
@@ -29,31 +28,26 @@ CREATE TABLE usuario (
 CREATE TABLE permissoes (
 	id_permissoes INT PRIMARY KEY AUTO_INCREMENT,
     nivel_usuario INT NOT NULL,
-    fk_usuario INT,
+    fk_usuario INT NOT NULL,
     FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE servidor (
-	id_servidor INT,
-    id_token int,
+	id_servidor INT PRIMARY KEY AUTO_INCREMENT,
     fornecedor VARCHAR(100) NOT NULL,
     modelo VARCHAR(100) NOT NULL,
-    numero_serie INT NOT NULL,
+    numero_serie VARCHAR(10) NOT NULL UNIQUE,
     status_servidor VARCHAR(45) NOT NULL,
     fk_unidade INT,
-    FOREIGN KEY (fk_unidade) REFERENCES unidade(id_unidade),
-    primary key (id_servidor, id_token)
+    FOREIGN KEY (fk_unidade) REFERENCES unidade(id_unidade)
 );
 
 CREATE TABLE token (
-	id_token INT,
-	fk_servidor INT,
-    fk_usuario INT,
+	id_token INT PRIMARY KEY AUTO_INCREMENT,
+	fk_servidor INT NOT NULL UNIQUE,
     token INT NOT NULL UNIQUE,
     end_check DATETIME NOT NULL,
-    PRIMARY KEY(id_token, fk_servidor, fk_usuario),
-    FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor),
-    FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
+    FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor)
 );
 
 CREATE TABLE componentes (
@@ -63,31 +57,21 @@ CREATE TABLE componentes (
     unidade_medida VARCHAR(45) NOT NULL,
     biblioteca VARCHAR(100) NOT NULL,
     parametros VARCHAR(100) NOT NULL,
-    id_servidorComponente INT,
     fk_servidor int,
     FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor)
 );
 
 CREATE TABLE registro (
-	id_info INT PRIMARY KEY AUTO_INCREMENT,
-    fk_servidor INT,
-    FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor),
-    fk_componente INT,
-    valor float,
-    FOREIGN KEY (fk_componente) REFERENCES componentes(id_componente)
+	id_registro INT PRIMARY KEY AUTO_INCREMENT,
+    fk_componentes INT NOT NULL,
+    FOREIGN KEY (fk_componentes) REFERENCES componentes(id_componente),
+    valor FLOAT NOT NULL,
+    Data_Hora DATETIME NOT NULL
 );
 
 CREATE TABLE Limite_Componente (
-	id_servidor INT,
-    id_componente INT,
-    nome VARCHAR(45),
-    tipo VARCHAR(45),
-    unidade_medida VARCHAR(45),
-    biblioteca VARCHAR(45),
-    parametros VARCHAR(45),
-    PRIMARY KEY (id_servidor, id_componente),
-    FOREIGN KEY (id_servidor) REFERENCES servidor(id_servidor),
-    FOREIGN KEY (id_componente) REFERENCES componentes(id_componente)
+	id_limite INT PRIMARY KEY AUTO_INCREMENT,
+    fk_componente INT NOT NULL UNIQUE,
+    FOREIGN KEY (fk_componente) REFERENCES componentes (id_componente),
+    limite_componente INT NOT NULL
 );
-
-
