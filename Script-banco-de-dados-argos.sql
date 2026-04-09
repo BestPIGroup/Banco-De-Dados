0efc411d6eb6c8b82a4f82a97a97e687777ad548
vazflavia1910 <flavia.vaz@sptech.school>
2026-04-08 23:28:51 -0300

    Alterações na modelagem do banco de dados e alterações no script de banco de dados

--- Script-banco-de-dados-argos.sql
+++ Script-banco-de-dados-argos.sql
@@ -1,4 +1,4 @@
-
+-- ARGOS GRUPO 10
 -- drop database projeto_Argos;
 
 CREATE DATABASE projeto_Argos;
@@ -8,73 +8,87 @@ CREATE TABLE unidade (
 	id_unidade INT PRIMARY KEY AUTO_INCREMENT,
     cod_unidade INT NOT NULL UNIQUE,
     cep CHAR(8) NOT NULL UNIQUE,
-    cidade VARCHAR(100) NOT NULL,
-    rua VARCHAR(100) NOT NULL,
-    bairro VARCHAR(100) NOT NULL,
-    estado VARCHAR(100) NOT NULL
+    cidade VARCHAR(45) NOT NULL,
+    rua VARCHAR(45) NOT NULL,
+    bairro VARCHAR(45) NOT NULL,
+    estado VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE usuario (
 	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    telefone CHAR(16) NOT NULL UNIQUE,
-    funcao VARCHAR(255) NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(45) NOT NULL,
+    telefone CHAR(12) NOT NULL UNIQUE,
+    funcao VARCHAR(45) NOT NULL,
     matricula INT NOT NULL UNIQUE,
+    permissoes VARCHAR (45),
     fk_responsavel INT,
-    fk_unidade INT,
-    FOREIGN KEY (fk_responsavel) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (fk_unidade) REFERENCES unidade(id_unidade)
-);
-
-CREATE TABLE permissoes (
-	id_permissoes INT PRIMARY KEY AUTO_INCREMENT,
-    nivel_usuario INT NOT NULL,
-    fk_usuario INT NOT NULL,
-    FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario)
+    CONSTRAINT fk_responsavel_usuario
+		FOREIGN KEY (fk_responsavel) 
+			REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE servidor (
 	id_servidor INT PRIMARY KEY AUTO_INCREMENT,
-    fornecedor VARCHAR(100) NOT NULL,
-    modelo VARCHAR(100) NOT NULL,
-    numero_serie VARCHAR(10) NOT NULL UNIQUE,
+    fornecedor VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    numero_serie VARCHAR(45) NOT NULL UNIQUE,
     status_servidor VARCHAR(45) NOT NULL,
     fk_unidade INT,
-    FOREIGN KEY (fk_unidade) REFERENCES unidade(id_unidade)
+    CONSTRAINT fk_unidade_servidor
+		FOREIGN KEY (fk_unidade) 
+			REFERENCES unidade(id_unidade)
 );
 
 CREATE TABLE token (
-	id_token INT PRIMARY KEY AUTO_INCREMENT,
-	fk_servidor INT NOT NULL UNIQUE,
+	id_token INT,
     token INT NOT NULL UNIQUE,
     end_check DATETIME NOT NULL,
-    FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor)
+    fk_usuario INT,
+    CONSTRAINT fk_usuario_token
+		FOREIGN KEY (fk_usuario) 
+			REFERENCES usuario(id_usuario),
+    fk_unidade INT NOT NULL UNIQUE,
+    CONSTRAINT fk_unidade_usuario
+		FOREIGN KEY (fk_unidade) 
+			REFERENCES unidade(id_unidade),
+	PRIMARY KEY (id_token, fk_usuario, fk_unidade)
 );
 
 CREATE TABLE componentes (
 	id_componente INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR (255) NOT NULL,
-    tipo VARCHAR(255) NOT NULL,
+    nome VARCHAR (45) NOT NULL,
+    tipo VARCHAR(45) NOT NULL,
     unidade_medida VARCHAR(45) NOT NULL,
-    biblioteca VARCHAR(100) NOT NULL,
-    parametros VARCHAR(100) NOT NULL,
-    fk_servidor int,
-    FOREIGN KEY (fk_servidor) REFERENCES servidor(id_servidor)
+    biblioteca VARCHAR(45) NOT NULL,
+    parametros VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE componente_servidor (
+	id_servidor INT NOT NULL UNIQUE,
+    CONSTRAINT id_servidor
+		FOREIGN KEY (id_servidor) 
+			REFERENCES servidor(id_servidor),
+    id_componente INT NOT NULL UNIQUE,
+    CONSTRAINT id_componente
+		FOREIGN KEY (id_componente) 
+			REFERENCES componentes(id_componente),
+    limite_componente INT NOT NULL,
+    PRIMARY KEY (id_servidor, id_componente)
 );
 
 CREATE TABLE registro (
-	id_registro INT PRIMARY KEY AUTO_INCREMENT,
-    fk_componentes INT NOT NULL,
-    FOREIGN KEY (fk_componentes) REFERENCES componentes(id_componente),
+	id_registro INT,
     valor FLOAT NOT NULL,
-    Data_Hora DATETIME NOT NULL
-);
-
-CREATE TABLE Limite_Componente (
-	id_limite INT PRIMARY KEY AUTO_INCREMENT,
-    fk_componente INT NOT NULL UNIQUE,
-    FOREIGN KEY (fk_componente) REFERENCES componentes (id_componente),
-    limite_componente INT NOT NULL
+    data_Hora DATETIME NOT NULL,
+    id_componente INT NOT NULL,
+    CONSTRAINT id_componente_servidor
+		FOREIGN KEY (id_componente) 
+			REFERENCES componente_servidor(id_componente),
+	id_servidor INT NOT NULL,
+    CONSTRAINT id_servidor_componente
+		FOREIGN KEY (id_servidor) 
+			REFERENCES componente_servidor(id_servidor),
+	PRIMARY KEY (id_registro, id_componente, id_servidor)
 );
