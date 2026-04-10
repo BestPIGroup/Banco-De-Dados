f98abab6d494f8a8fd81e743a5ed902ce1dea507
vazflavia1910 <flavia.vaz@sptech.school>
2026-04-10 11:15:58 -0300

    Alteração no script de banco de dados e na modelagem de banco de dados

--- Script-banco-de-dados-argos.sql
+++ Script-banco-de-dados-argos.sql
@@ -15,54 +15,45 @@ CREATE TABLE unidade (
 );
 
 CREATE TABLE usuario (
-	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE,
-    senha VARCHAR(45) NOT NULL,
-    telefone CHAR(12) NOT NULL UNIQUE,
+	id_usuario INT NOT NULL,
+    fk_unidade INT NOT NULL,
+    CONSTRAINT fk_unidade_usuario
+		FOREIGN KEY (fk_unidade)
+			REFERENCES unidade(id_unidade),
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(60) NOT NULL,
+    telefone CHAR(11) NOT NULL,
     funcao VARCHAR(45) NOT NULL,
-    matricula INT NOT NULL UNIQUE,
-    permissoes VARCHAR (45),
+    identificador INT NOT NULL UNIQUE,
     fk_responsavel INT,
     CONSTRAINT fk_responsavel_usuario
 		FOREIGN KEY (fk_responsavel) 
-			REFERENCES usuario(id_usuario)
+			REFERENCES usuario(id_usuario),
+	PRIMARY KEY (id_usuario, fk_unidade)
 );
 
 CREATE TABLE servidor (
 	id_servidor INT PRIMARY KEY AUTO_INCREMENT,
     fornecedor VARCHAR(45) NOT NULL,
     modelo VARCHAR(45) NOT NULL,
-    numero_serie VARCHAR(45) NOT NULL UNIQUE,
+    endereco_mac CHAR(12) NOT NULL UNIQUE,
     status_servidor VARCHAR(45) NOT NULL,
+    CONSTRAINT status_servidor 
+		CHECK(status_servidor IN ('Manutenção', 'Inativo', 'Ativo')),
     fk_unidade INT,
     CONSTRAINT fk_unidade_servidor
 		FOREIGN KEY (fk_unidade) 
 			REFERENCES unidade(id_unidade)
 );
 
-CREATE TABLE token (
-	id_token INT,
-    token INT NOT NULL UNIQUE,
-    end_check DATETIME NOT NULL,
-    fk_usuario INT,
-    CONSTRAINT fk_usuario_token
-		FOREIGN KEY (fk_usuario) 
-			REFERENCES usuario(id_usuario),
-    fk_unidade INT NOT NULL UNIQUE,
-    CONSTRAINT fk_unidade_usuario
-		FOREIGN KEY (fk_unidade) 
-			REFERENCES unidade(id_unidade),
-	PRIMARY KEY (id_token, fk_usuario, fk_unidade)
-);
-
-CREATE TABLE componentes (
+CREATE TABLE componente (
 	id_componente INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (45) NOT NULL,
     tipo VARCHAR(45) NOT NULL,
     unidade_medida VARCHAR(45) NOT NULL,
     biblioteca VARCHAR(45) NOT NULL,
-    parametros VARCHAR(45) NOT NULL
+    parametro VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE componente_servidor (
@@ -73,15 +64,14 @@ CREATE TABLE componente_servidor (
     id_componente INT NOT NULL UNIQUE,
     CONSTRAINT id_componente
 		FOREIGN KEY (id_componente) 
-			REFERENCES componentes(id_componente),
+			REFERENCES componente(id_componente),
     limite_componente INT NOT NULL,
-    PRIMARY KEY (id_servidor, id_componente)
+    PRIMARY KEY (id_servidor, id_componente),
+    exibir BOOLEAN
 );
 
 CREATE TABLE registro (
 	id_registro INT,
-    valor FLOAT NOT NULL,
-    data_Hora DATETIME NOT NULL,
     id_componente INT NOT NULL,
     CONSTRAINT id_componente_servidor
 		FOREIGN KEY (id_componente) 
@@ -90,5 +80,7 @@ CREATE TABLE registro (
     CONSTRAINT id_servidor_componente
 		FOREIGN KEY (id_servidor) 
 			REFERENCES componente_servidor(id_servidor),
+    valor FLOAT NOT NULL,
+    data_Hora DATETIME NOT NULL,
 	PRIMARY KEY (id_registro, id_componente, id_servidor)
 );
